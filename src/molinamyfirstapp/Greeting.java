3b6692ad0d98802111d295e3de64992b51d6577e
BarryMolina <bazzaboy@gmail.com>
2019-09-08 20:25:14 -0500

    Add MolinaMyFirstApp

---
+++ src/molinamyfirstapp/Greeting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package molinamyfirstapp;
+
+/**
+ *
+ * @author itsup
+ */
+public class Greeting {
+    
+    public Greeting()
+    {
+        System.out.println("Hello World! \n");
+        System.out.println("My name is: \tBarry Molina");
+        System.out.println("My age is: \t28");
+    }
+    
+}
---
+++ src/molinamyfirstapp/MolinaMyFirstApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package molinamyfirstapp;
+
+/**
+ *
+ * @author itsup
+ */
+public class MolinaMyFirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Greeting g = new Greeting();
+        
+    }
+    
+}
